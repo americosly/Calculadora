0f0927354d8f39dd3c867e77494c31aed9b75fb0
americosly <americo_tss@hotmail.com>
2022-05-20 19:01:40 -0300

    iniciando projeto

---
+++ projeto/src/main/Calculator.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './Calculator.css'
+
+export default class Calculator extends Component {
+    render() {
+        console.log("oi")
+        return (
+            <div className = "calculator">
+
+            </div>
+        )
+    }
+}
